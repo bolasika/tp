b290bad239e72656f3fc437a49c3323ce5c89748
eejon <eejonlee@gmail.com>
2026-03-12 11:14:17 +0800

    feat: Add find command for NAB

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -15,7 +15,8 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
-    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d contacts listed!";
+    public static final String MESSAGE_PERSONS_NONE_FOUND = "No matching contacts found!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
 
@@ -40,9 +41,9 @@ public class Messages {
                 .append("; Phone: ")
                 .append(person.getPhone())
                 .append("; Email: ")
-                .append(person.getEmail().map(email -> email.toString()))
+                .append(person.getEmail().map(email -> email.toString()).orElse(""))
                 .append("; Address: ")
-                .append(person.getAddress().map(address -> address.toString()))
+                .append(person.getAddress().map(address -> address.toString()).orElse(""))
                 .append("; Tags: ");
         person.getTags().forEach(builder::append);
         return builder.toString();
