873d3c14dd3eeb318a503f65339f122b6fe9db30
David <53869530+breezuu@users.noreply.github.com>
2026-04-10 07:35:44 +0800

    Fix Checkstyle issue in import command test file

--- src/test/java/seedu/address/logic/commands/ImportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ImportCommandTest.java
@@ -22,8 +22,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.commons.util.PhotoStorageUtil;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
